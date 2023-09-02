e95c578c9e5839229ca8381fc2c9df05bd664410
Sveta <seta2020s@gmail.com>
2023-09-02 16:35:44 +0700

    написала код

---
+++ task/Program.cs
@@ -0,0 +1,36 @@
+﻿void ShowArray(string[] array)
+{
+    Console.WriteLine("Результат:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+
+string[] FilterArray(string[] inputArray)
+{
+    int count = 0;
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            inputArray[count] = inputArray[i];
+            count++;
+        }
+    }
+
+    string[] resultArray = new string[count];
+    Array.Copy(inputArray, resultArray, count);
+
+    return resultArray;
+}
+
+Console.WriteLine("Введите элементы массива (через запятую и пробел):");
+string input = Console.ReadLine();
+
+string[] inputArray = input.Split(new[] { ", " }, StringSplitOptions.None);
+
+string[] resultArray = FilterArray(inputArray);
+
+ShowArray(resultArray);
